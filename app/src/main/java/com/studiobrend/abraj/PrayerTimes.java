2ca774c11ae16f9350675521fa1c65bc71a943c4
Goran Djokovic <serbonart@gmail.com>
2024-03-05 00:16:00 +0100

    Update

--- app/src/main/java/com/studiobrend/abraj/PrayerTimes.java
+++ app/src/main/java/com/studiobrend/abraj/PrayerTimes.java
@@ -7,18 +7,66 @@ public class PrayerTimes {
     private String asr;
     private String maghrib;
     private String isha;
-    private String midnight;
 
-    public PrayerTimes(String fajr, String sunrise, String dhuhr, String asr, String maghrib, String isha, String midnight) {
+    // Konstruktor koji prima vremena molitvi
+    public PrayerTimes(String fajr, String sunrise, String dhuhr, String asr, String maghrib, String isha) {
         this.fajr = fajr;
         this.sunrise = sunrise;
         this.dhuhr = dhuhr;
         this.asr = asr;
         this.maghrib = maghrib;
         this.isha = isha;
-        this.midnight = midnight;
     }
 
-    // Dodajte gettere i eventualno settere
-}
+    // Getere
+    public String getFajr() {
+        return fajr;
+    }
 
+    public String getSunrise() {
+        return sunrise;
+    }
+
+    public String getDhuhr() {
+        return dhuhr;
+    }
+
+    public String getAsr() {
+        return asr;
+    }
+
+    public String getMaghrib() {
+        return maghrib;
+    }
+
+    public String getIsha() {
+        return isha;
+    }
+
+    // Setere
+    public void setFajr(String fajr) {
+        this.fajr = fajr;
+    }
+
+    public void setSunrise(String sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public void setDhuhr(String dhuhr) {
+        this.dhuhr = dhuhr;
+    }
+
+    public void setAsr(String asr) {
+        this.asr = asr;
+    }
+
+    public void setMaghrib(String maghrib) {
+        this.maghrib = maghrib;
+    }
+
+    public void setIsha(String isha) {
+        this.isha = isha;
+    }
+
+    // Možete dodati dodatne metode za manipulaciju ili prikaz vremena molitvi
+}
