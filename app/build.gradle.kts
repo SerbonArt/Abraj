bba735c65120b917ca6db0c0dee0bf5bf348611e
Goran Djokovic <serbonart@gmail.com>
2024-03-07 22:11:09 +0100

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     }
 
     buildTypes {
-        getByName("release") {
+        release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
@@ -26,30 +26,21 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
 }
 
 dependencies {
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("io.reactivex.rxjava3:rxjava:3.1.3")
     implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
-    implementation("androidx.preference:preference:1.2.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.readystatesoftware.sqliteasset:sqliteassethelper:2.0.1")
-    implementation(files("libs/prayer_time_library.jar"))
-    implementation("androidx.core:core:1.10.1")
-    implementation("androidx.fragment:fragment:1.6.1")
-    implementation("androidx.multidex:multidex:2.0.1")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.jakewharton.threetenabp:threetenabp:1.3.1")
-    implementation("com.jakewharton.timber:timber:4.7.1")
-
+    implementation("com.google.android.gms:play-services-location:18.0.0")
 
     testImplementation("junit:junit:4.13.2")
-
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
